e450a9d57c6ea49b07fa9dfbf8f21d65f86d8183
alex <corto_soda@noos.fr>
2018-06-28 15:41:23 +0200

    conflit conflictuel.

--- index.php
+++ index.php
@@ -26,5 +26,6 @@
     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
     non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
+    <h2>C'est mon code le meilleur.</h2>
 </body>
 </html>
