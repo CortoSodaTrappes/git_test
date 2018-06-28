9bba389647297472e22b5cd9bc3e0f25802dc164
alex <corto_soda@noos.fr>
2018-06-28 11:28:45 +0200

    ajout de style

--- index.php
+++ index.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>TEST</title>
+    <style>
+    h1{
+        position:fixed;
+        font-size:2em;
+        color:red;
+    }
+    </style>
 </head>
 <body>
     <h1>Ceci est un test</h1>
